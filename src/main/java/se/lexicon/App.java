0f0fec81cf2cb8776cd8937762141619e191610e
ZicoCoder <Azzoug.zackaria@gmail.com>
2025-10-30 06:34:19 +0100

    Run main to verify NameRepository

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -7,7 +7,12 @@ public class App {
         int size = NameRepository.getSize();
         System.out.println(size);
         NameRepository.setNames(new String[]{"Erik Svensson", "Mehrdad Javan"});
-        System.out.println(NameRepository.getSize());
-        // call more methods as needed
+        System.out.println(NameRepository.getSize());System.out.println("Find 'Erik Svensson': " + NameRepository.find("Erik Svensson"));
+        System.out.println("Add duplicate 'Erik Svensson': " + NameRepository.add("Erik Svensson"));
+        System.out.println("Add new 'Sara Svensson': " + NameRepository.add("Sara Svensson"));
+        System.out.println("Size after add: " + NameRepository.getSize());
+        System.out.println("Remove 'Mehrdad Javan': " + NameRepository.remove("Mehrdad Javan"));
+        System.out.println("Final size: " + NameRepository.getSize());
+
     }
 }
